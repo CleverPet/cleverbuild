3cf2ced7486058851634f20b45baaf99e45ec945
rrizun <rrizun@gmail.com>
2016-06-24 11:25:07 -0700

    include sort-human

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(name="clevertools", scripts=["cleverbuild", "cleverdeploy"])
+setuptools.setup(name="clevertools", scripts=["cleverbuild", "cleverdeploy", "sort-human"])
